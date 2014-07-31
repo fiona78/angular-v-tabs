36e5df20f81b353904a8f5a49bf3cfd0b5d0954a
Fiona Przybylski <fiona@clearbooks.co.uk>
2014-07-31 11:57:09 +0100

    tabs

--- js/tabs.js
+++ js/tabs.js
@@ -2,84 +2,47 @@ angular.module( 'vTabs', [] )
 
 //tabs directive
 
-.directive('vTabs', function(){
-
-
+.directive('tabs', function(){
     return{
-       //require:['ngModel', '?^vTab'], //array of required driecrive controllers
-       require: '^vTabs',
+       require: '?^tab',
+        restrict: 'EA',
+        //replace:true,
+
+        controller: function($scope,  $element) {
+        },
+
+        link: function( scope, element, attrs, tabCtrl ) {
+            if ( tabCtrl ) {
+                console.log( tabCtrl.getTitle() );
+            }
+        }
+    };//end of return
+
+})//end of directive
+
+
+.directive('tab', function(){
+    return{
+       require: '^tabs',
         restrict: 'EA',
         //replace:true,
         scope: {
             title: "@title"
-            //binds to the title attribute
-
         },
 
         controller: function($scope,  $element) {
             this.getTitle = function() {
                 return $scope.title;
             }
-   
-//                console.log($scope.title);
-                
-    
         },
 
         link: function( scope, element, attrs, vTabsCtrl ) { 
-            console.log( scope.title, vTabsCtrl.getTitle() );
-
-        //console.log(vTabsCtrl);
-          
-
-    
-
         }
-
-
-
     };//end of return
 
 })//end of directive
 
-/*.directive('vTab', function(){
-
-
-    return{
-       require:['^vTabs'], //array of required driecrive controllers
-        restrict: 'EA',
-        scope: true,
-        controller: function($scope, $element, $attrs) {
-       
-            this.scope = $scope;
-            $scope.$element = $element;
-
-            //$scope.$modelValue = null; // Model value for node;
-           // $scope.$parentNodeScope = null; // uiTreeNode Scope of parent node;
-
-
-        },
-        link: function(scope, element, attrs, vTabsCtrl) { 
-
-        /*scope.testing = "hello";*/
-        //scope.testing = "hello";
-        /*console.log(vTabsCtrl)
-
-
-
-
-        }
-
-
-
-
-
-
-    };//end of return
-
-})**////end of directive
-
-
+;
 
 
 
